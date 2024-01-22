5d3af44ff21b5a0db64cf0e0b0f32ed97aa677d9
Егор Шульга <egorsulga0@gmail.com>
2024-01-23 00:05:04 +0300

    1) обычгые шахматы готовы 2) шахматы 2 цыкла готово

--- Geometry_2/main.cpp
+++ Geometry_2/main.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
 using namespace std;
 
-#define SQUARE
+//#define SQUARE
 //#define TRINGLE_1
 //#define TRINGLE_2
 //#define TRINGLE_3
 //#define TRINGLE_4
-void main()
+
+#define UP_LEFT_AGNLE	(char)218
+#define UP_RAGHT_AGNLE	(char)191
+#define LOW_LEFT_AGNLE	(char)192
+#define LOW_RAGHT_AGNLE	(char)217
+#define WHITE_BOX		(char)219	<< (char)219
+
+#define HORIZONTAL		(char)196	<< (char)196
+#define VERTICAL		(char)179	
+
+//#define CHESS_WINDOW
+//#define CHESS_PROSTO_CHESS
+#define CHESS_TWO_CYCLE
+
+int main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "ru");
 	int n;
 	cout << "Введите размер фигуры: "; cin >> n;
 
@@ -63,15 +77,89 @@ void main()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			/*if ((i + j) % 2 == 0) cout << "+ ";
-			else cout << "- ";*/
+			if ((i + j) % 2 == 0) cout << "+ ";
+			else cout << "- ";
 
 			//if (i % 2 == j % 2) cout << "+ "; else cout << "- ";
 			//i % 2 == j % 2 ? cout << "+ ": cout << "- ";
-			cout << (i % 2 == j % 2 ? "+ " : "- ");
+			//cout << (i % 2 == j % 2 ? "+ " : "- ");
+		}
+		cout << endl;
+	}
+#endif
+
+	setlocale(LC_ALL, "C");
+
+#ifdef CHESS_WINDOW	
+	n++;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			if (i == 0 && j == 0) cout << UP_LEFT_AGNLE;
+			else if (i == 0 && j == n) cout << UP_RAGHT_AGNLE;
+			else if (i == n && j == 0) cout << LOW_LEFT_AGNLE;
+			else if (i == n && j == n) cout << LOW_RAGHT_AGNLE;
+			else if (i == 0 || i == n) cout << HORIZONTAL;
+			else if (j == 0 || j == n) cout << VERTICAL;
+			else if (i % 2 == j % 2) cout << WHITE_BOX;
+			else cout << "  ";
+		}
+		cout << endl;
+	}
+
+#endif
+
+#ifdef CHESS_PROSTO_CHESS
+	setlocale(LC_ALL, "ru");
+
+	int j;
+	cout << "Введите размер внутреннего квадрата: "; cin >> j;
+
+	for (int i = 0; i < n; i++)						// циклы i и v для размера поля (фигуры)
+	{
+		for (int f = 0; f < j; f++)
+		{
+			for (int v = 0; v < n; v++)				 // циклы f и h для внутреннего квадрата и его размера
+			{
+				for (int h = 0; h < j; h++)
+				{
+					if (i % 2 == v % 2)				// если их остатки равны * 
+					{
+						cout << "* ";
+					}
+					else
+					{
+						cout << "  ";
+					}
+				}
+			}
+			cout << endl;
+		}
+	}
+#endif
+
+#ifdef CHESS_TWO_CYCLE
+
+	setlocale(LC_ALL, "ru");
+	int j;
+	cout << "j = "; cin >> j;
+
+
+	for (int i = 0; i < n * n; i++)	// вертикаль
+	{
+		for (int f = 0; f < j * j; f++)	// горизонталь 
+		{
+			if (((f / j) + (i / n)) % 2 == 0 )	// 
+				cout << "* ";
+			else
+				cout << "  ";
+
 		}
 		cout << endl;
 	}
 #endif
 
 }
+
+					
